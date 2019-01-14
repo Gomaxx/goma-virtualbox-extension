7aaf56245fbc9d4118a7c92c922c1f3cd6ee846f
Gomaxx <oma.onen@gmail.com>
2019-01-14 09:54:02 +0800

    服务器地址接收输入

--- goma-virtualbox-extension-client/src/main/java/cc/eoma/virtualbox/StartApplication.java
+++ goma-virtualbox-extension-client/src/main/java/cc/eoma/virtualbox/StartApplication.java
@@ -7,20 +7,27 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
-public class StartApplication {
-  public static void main(String[] args) throws Exception {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-    EventLoopGroup group = new NioEventLoopGroup();
-    Bootstrap bootstrap = new Bootstrap()
-        .group(group)
-        .channel(NioSocketChannel.class)
-        .handler(new MyChannelInitializer());
-    Channel channel = bootstrap.connect("192.168.1.95", 35888).sync().channel();
-    System.out.print("请输入要启动监听数量: ");
-    Integer count = Integer.valueOf(((char) System.in.read()) + "");
-    for (int i = 0; i < count; i++) {
-      new Thread(new TrayIconMonitor(channel)).start();
-      Thread.sleep(10000L);
+public class StartApplication {
+    public static void main(String[] args) throws Exception {
+
+        System.out.print("请输入服务器IP: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String address = br.readLine();
+
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap()
+                .group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new MyChannelInitializer());  // 192.168.43.204   192.168.1.95    10.190.41.179
+        Channel channel = bootstrap.connect(address, 35888).sync().channel();
+        System.out.print("请输入要启动监听数量: ");
+        Integer count = Integer.valueOf(((char) System.in.read()) + "");
+        for (int i = 0; i < count; i++) {
+            new Thread(new TrayIconMonitor(channel)).start();
+            Thread.sleep(10000L);
+        }
     }
-  }
 }
